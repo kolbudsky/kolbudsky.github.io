325a20e1a1f0c1f7befb84787a831ecc62776976
kolbudsky <33987566+kolbudsky@users.noreply.github.com>
2017-12-19 17:56:48 +0200

    Add files via upload

---
+++ myDesign/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("#comments-slider").owlCarousel({
+        items: 1,
+        nav: true,
+        slideSpeed: 1000,
+        paginationSpeed: 1000,
+        autoplay: true,
+        loop: true,
+        dotsEach: true
+        
+    });
+});
